91b3eb56634de59ef7f3ded0c6d34d4e32c1bc0d
Rishabh Kumar <rishabhkumar@Rishabhs-MacBook-Pro.local>
2023-12-26 16:17:42 +0530

    Added Currency formatter

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,17 +1,20 @@
-export default function MealItem({data}) {
+import {currencyFormatter} from "../utils/formatting.js";
+export default function MealItem({ data }) {
   return (
-    <li className='meal-item'>
-        <article>
-            <img src={`http://localhost:3000/${data.image}`} alt={data.name}/>
-            <div>
-                <h3>{data.name}</h3>
-                <p className='meal-item-price'>{data.price}</p>
-                <p className='meal-item-description'>{data.description}</p>
-            </div>
-            <p className='meal-item-actions'>
-                <button>Add to Cart</button>
-            </p>
-        </article>
+    <li className="meal-item">
+      <article>
+        <img src={`http://localhost:3000/${data.image}`} alt={data.name} />
+        <div>
+          <h3>{data.name}</h3>
+          <p className="meal-item-price">
+            {currencyFormatter.format(data.price)}
+          </p>
+          <p className="meal-item-description">{data.description}</p>
+        </div>
+        <p className="meal-item-actions">
+          <button>Add to Cart</button>
+        </p>
+      </article>
     </li>
-  )
+  );
 }
